69117b6a1bda78ca7b39b9ea28d89d80d13a89e4
Riboshom <riboshom@hotmail.com>
2017-09-03 18:05:21 -0400

    Time rows using a timer thread that sleeps in parallel

--- src/playback_timer.rs
+++ src/playback_timer.rs
@@ -30,6 +30,10 @@ impl<'a> Timer<'a> {
 	pub fn play_routines(&mut self, routines: &Vec<(Routine, Instrument)>) {
 		let row_duration = (60000f32/(self.lines_par_beat * self.beats_per_minute as f32)) as u64;
 
+		let timer_thread = thread::spawn(move || {
+			thread::sleep(Duration::from_millis(row_duration));
+		});
+
 		// Only play first tick for now, forever
 		for (channel_idx, &(routine, instrument)) in routines.iter().enumerate() {
 			let tick_result = routine.tick_value(0);
@@ -43,8 +47,7 @@ impl<'a> Timer<'a> {
 			}
 		}
 
-		// Let's be optimistic and assume for now that IO and calculations take 0ms
-		thread::sleep(Duration::from_millis(row_duration));
+		timer_thread.join();
 	}
 
 	pub fn set_speed(&mut self, speed: i32) {
