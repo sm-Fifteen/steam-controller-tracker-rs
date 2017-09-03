0402d9fcf4c3b2421ac9089153a5a070dd4e7be4
Riboshom <riboshom@hotmail.com>
2017-09-02 22:08:04 -0400

    parse_row now fills a vec of Routine-Instrument tuples

--- src/module_parser.rs
+++ src/module_parser.rs
@@ -21,7 +21,7 @@ pub fn parse_module(config: &mut AppConfig) {
 		channel_filter: &config.channel_filter,
 	};
 
-	let mut routines:Vec<Routine> = vec![Routine::StopNote; config.num_channels as usize];
+	let mut routines:Vec<(Routine, Instrument)> = vec![(Routine::StopNote, row_config.instruments[0]); config.num_channels as usize];
 
 	while let Some(mut pattern) = config.module.get_pattern_by_order(next_pattern_order) {
 		while let Some(mut row) = pattern.get_row_by_number(next_row_num) {
@@ -34,21 +34,20 @@ pub fn parse_module(config: &mut AppConfig) {
 	}
 }
 
-// Missing instrument refs
-fn parse_row(row: &mut Row, config: &RowParsingConfig, routines: &mut Vec<Routine>) {
+fn parse_row(row: &mut Row, config: &RowParsingConfig, routines: &mut Vec<(Routine, Instrument)>) {
 	// TODO : Check global effects (set speed/tempo, break pattern, goto order)
 	// FIXME : Not ideal to play same channel multiple times
 	for (idx, channel_num) in config.channel_filter.iter().enumerate() {
 		let mut cell = row.get_cell_by_channel(*channel_num).expect(&format!("Not cell at channel {}", *channel_num));
-		let routine:&mut [Routine] = &mut routines[idx..idx+1];
+		let routine:&mut [(Routine, Instrument)] = &mut routines[idx..idx+1];
 
 		if let Ok(cell_data) = cell.get_data() {
-			cell_to_routine(&cell_data, routine);
+			cell_to_routine(&cell_data, config.instruments, routine);
 		}
 	}
 }
 
-fn cell_to_routine(cell_data: &ModCommand, out_routines: &mut [Routine]) {
+fn cell_to_routine<'a>(cell_data: &ModCommand, instruments: &[Instrument], out_routines: &mut [(Routine, Instrument)]) {
 	let new_routine = match cell_data.note {
 		Note::Note(semitone_idx) => {
 			let note = ::music::Note::new(semitone_idx as i16);
@@ -59,7 +58,8 @@ fn cell_to_routine(cell_data: &ModCommand, out_routines: &mut [Routine]) {
 	};
 
 	if new_routine.is_some() {
+		let instrument = instruments[cell_data.instr as usize];
 		let new_routine = new_routine.unwrap();
-		out_routines[0] = new_routine;
+		out_routines[0] = (new_routine, instrument);
 	}
 }
